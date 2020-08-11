e186fd3adf29c5fdf48d444aee46535e6e51f8e3
nadim <smnadim21@gmail.com>
2020-08-11 12:32:57 +0600

    containers added  by smnadim21

---
+++ customview/src/main/java/com/smnadim21/customview/containers/ExpandableHeightListView.java
@@ -0,0 +1,59 @@
+package com.smnadim21.customview.containers;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+/**
+ * Created by d3stR0y3r on 13/07/2018.
+ */
+public class ExpandableHeightListView extends ListView {
+
+    boolean expanded = false;
+
+    public ExpandableHeightListView(Context context) {
+        super(context);
+    }
+
+    public ExpandableHeightListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ExpandableHeightListView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public ExpandableHeightListView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    public boolean isExpanded() {
+        return expanded;
+    }
+
+    @Override
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        // HACK! TAKE THAT ANDROID!
+        if (isExpanded()) {
+            // Calculate entire height by providing a very large height hint.
+            // View.MEASURED_SIZE_MASK represents the largest height possible.
+            int expandSpec = MeasureSpec.makeMeasureSpec(MEASURED_SIZE_MASK,
+                    MeasureSpec.AT_MOST);
+            super.onMeasure(widthMeasureSpec, expandSpec);
+
+            ViewGroup.LayoutParams params = getLayoutParams();
+            params.height = getMeasuredHeight();
+        } else {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        }
+    }
+
+    public void setExpanded(boolean expanded) {
+        this.expanded = expanded;
+    }
+
+}
