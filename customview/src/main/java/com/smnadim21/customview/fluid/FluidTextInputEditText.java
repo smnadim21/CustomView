983a369dfe5df77af07b07c1f5930cfbc023e609
nadim <smnadim21@gmail.com>
2020-10-23 03:46:25 +0600

    fluid fix3  by smnadim21

--- customview/src/main/java/com/smnadim21/customview/fluid/FluidTextInputEditText.java
+++ customview/src/main/java/com/smnadim21/customview/fluid/FluidTextInputEditText.java
@@ -32,6 +32,6 @@ public class FluidTextInputEditText extends TextInputEditText {
     float getTextSizeFactor() {
         int width = Resources.getSystem().getDisplayMetrics().widthPixels;
         int height = Resources.getSystem().getDisplayMetrics().heightPixels;
-        return ((float) height / width) * 2;
+        return ((float) height / width) * 2.5f;
     }
 }
